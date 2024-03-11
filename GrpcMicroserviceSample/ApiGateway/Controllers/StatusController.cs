0512d587cc509c9686ba6193b46a1a70f111ab0f
Kevin Hayes Anderson <kevinhayesanderson@gmail.com>
2024-03-11 08:47:28 +0530

    Sample microservice grpc communication

---
+++ GrpcMicroserviceSample/ApiGateway/Controllers/StatusController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ApiGateway.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StatusController : ControllerBase
+    {
+        private readonly IGrpcStatusClient client;
+
+        public StatusController(IGrpcStatusClient client)
+        {
+            this.client = client;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<ClientStatusModel>> GetAllStatuses()
+        {
+            return await client.GetAllStatuses();
+        }
+
+        [HttpGet("{clientName}")]
+        public async Task<ClientStatusModel> GetClientStatus(string clientName)
+        {
+            return await client.GetClientStatus(clientName);
+        }
+
+        [HttpPost("{clientName}/{status}")]
+        public async Task<bool> UpdateClientStatus(string clientName, ClientStatus status)
+        {
+            return await client.UpdateClientStatus(clientName, status);
+        }
+    }
+}
