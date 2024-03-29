aa265ceafca1eefc0a90c246b3d75a7a6f0f03fb
Kevin Hayes Anderson <kevinhayesanderson@gmail.com>
2024-03-29 12:19:21 +0530

    Versioning

---
+++ GrpcVersioning/GrpcClient/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Please enter gRPC server address:");
+var serverUrl = Console.ReadLine();
+var client = new Stats.V1.Status.StatusClient(GrpcChannel.ForAddress(serverUrl));
+
+Console.WriteLine("Please enter client name:");
+var clientName = Console.ReadLine();
+
+Console.WriteLine("Please enter client description:");
+var clientDescription = Console.ReadLine();
+
+var response = await client.GetStatusAsync(new Stats.V1.StatusRequest
+{
+    ClientName = clientName,
+    ClientDescription = clientDescription,
+    Allowed = true
+});
+
+Console.WriteLine($"Server name: {response.ServerName}");
+Console.WriteLine($"Server description:{ response.ServerDescription}");
+Console.WriteLine($"Number of connections:{ response.NumberOfConnections}");
+Console.WriteLine($"CPU usage: {response.CpuUsage}");
+Console.WriteLine($"Memory usage: {response.MemoryUsage}");
+Console.WriteLine($"Errors logged: {response.ErrorsLogged}");
+Console.WriteLine($"Catastrophic failures logged:{ response.CatastrophicFailuresLogged}");
+Console.WriteLine($"Active: {response.Active}");
+Console.ReadKey();
---
+++ GrpcVersioning/GrpcServer/Services/StatusServiceV1.cs
@@ -0,0 +1,29 @@
+﻿using Grpc.Core;
+using Stats.V1;
+
+namespace GrpcServer
+{
+    public class StatusServiceV1 : Stats.V1.Status.StatusBase
+    {
+        public override Task<StatusResponse> GetStatus(StatusRequest request, ServerCallContext context)
+        {
+            Console.WriteLine($"Client name is {request.ClientName}");
+            Console.WriteLine($"Client description is {request.ClientDescription}");
+            Console.WriteLine($"Is client ready? {request.Ready}");
+            Console.WriteLine($"Is client authorized? {request.Authorized}");
+
+            var random = new Random();
+            return Task.FromResult(new StatusResponse
+            {
+                ServerName = "TestServer",
+                ServerDescription = "This is a test server that is used for generating status metrics",
+                NumberOfConnections = random.Next(),
+                CpuUsage = random.NextDouble() * 100,
+                MemoryUsage = random.NextDouble() * 100,
+                ErrorsLogged = (ulong)random.Next(),
+                CatastrophicFailuresLogged = (uint)random.Next(),
+                Active = true
+            });
+        }
+    }
+}
