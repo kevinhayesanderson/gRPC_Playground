f36a79aff3c9e5094c2d13f2e9d6a4b8ceca2fa8
Kevin Hayes Anderson <kevinhayesanderson@gmail.com>
2024-04-24 04:22:18 +0530

    proxy load balancing

---
+++ GrpcLoadBalancing/Http2Proxy/Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var proxyBuilder = builder.Services.AddReverseProxy();
+
+proxyBuilder.LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapReverseProxy();
+
+app.Run();
